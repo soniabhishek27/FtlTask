7fcb3884fce55e547aedd3b24b822326b86d367d
sonabhishek27 <abhisheksoni3216@gmail.com>
2020-09-02 12:56:52 +0530

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,32 @@
-
 import 'package:flutter/material.dart';
 import 'package:ftltask/screens/HomeScreen.dart';
 import 'package:ftltask/screens/OperationsScreen.dart';
 import 'package:ftltask/utils/ThemeChanger.dart';
 import 'package:provider/provider.dart';
 
-void main()
-{
+void main() {
   runApp(
-      ChangeNotifierProvider<ThemeState>
-        (
-        create: (context)=> ThemeState(),
-
-        child: MyApp(),
-
-      ),
+    ChangeNotifierProvider<ThemeState>(
+      create: (context) => ThemeState(),
+      child: MyApp(),
+    ),
   );
-
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: Provider.of<ThemeState>(context).theme == ThemeType.DARK
-      ? ThemeData.dark()
+          ? ThemeData.dark()
           : ThemeData.light(),
 
-        title: 'To DO',
+      title: 'To DO',
       debugShowCheckedModeBanner: false,
-
-
-      // theme: ThemeData.light(),
-
       initialRoute: HomeScreen.id,
       routes: {
-
-          HomeScreen.id : (context) => HomeScreen(),
-          // OperationsScreen.id : (context) => OperationsScreen(""),
+        HomeScreen.id: (context) => HomeScreen(),
+        // OperationsScreen.id : (context) => OperationsScreen(""),
       },
     );
   }
